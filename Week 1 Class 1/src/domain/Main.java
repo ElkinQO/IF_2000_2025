48e324e3ddff6e981aa4168d9f15f809459442c9
Elkin Quiros <olivareselkin9@gmail.com>
2025-08-14 11:26:58 -0600

    Mi primer Commit en Java

---
+++ Week 1 Class 1/src/domain/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author oliva
+ */
+public class Main {
+    public static void main (String args []){
+        Person person = new Person();
+        
+        System.out.println(toString());
+        
+        
+    }
+    
+}
